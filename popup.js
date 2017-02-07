bfd298277beab6c653f7e6ccda4cfa38812bbdbc
Rishabh Jain <mailrishabh1403@gmail.com>
2017-02-07 14:19:42 +0530

    simple popup created

--- popup.js
+++ popup.js
@@ -1,97 +1,171 @@
+var allSvg = {
+	$successSvg: ' <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"' +
+		'viewBox="0 0 507.2 507.2" style="enable-background:new 0 0 507.2 507.2;width:30px;height:30px;" xml:space="preserve">' +
+		'<circle style="fill:#32BA7C;" cx="253.6" cy="253.6" r="253.6"/>' +
+		'<path style="fill:#0AA06E;" d="M188.8,368l130.4,130.4c108-28.8,188-127.2,188-244.8c0-2.4,0-4.8,0-7.2L404.8,152L188.8,368z"/>' +
+		'<g><path style="fill:#FFFFFF;" d="M260,310.4c11.2,11.2,11.2,30.4,0,41.6l-23.2,23.2c-11.2,11.2-30.4,11.2-41.6,0L93.6,272.8' +
+		'		c-11.2-11.2-11.2-30.4,0-41.6l23.2-23.2c11.2-11.2,30.4-11.2,41.6,0L260,310.4z"/>' +
+		'	<path style="fill:#FFFFFF;" d="M348.8,133.6c11.2-11.2,30.4-11.2,41.6,0l23.2,23.2c11.2,11.2,11.2,30.4,0,41.6l-176,175.2' +
+		'		c-11.2,11.2-30.4,11.2-41.6,0l-23.2-23.2c-11.2-11.2-11.2-30.4,0-41.6L348.8,133.6z"/>' +
+		'</g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>',
+
+	$closeSvg: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" style="width:10px;height:10px;" viewBox="0 0 21.9 21.9" xmlns:xlink="http://www.w3.org/1999/xlink" enable-background="new 0 0 21.9 21.9">' +
+		'<path d="M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z"/>' +
+		'</svg>',
+	$warningSvg: '<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"' +
+		'viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;width:30px;height:30px;" xml:space="preserve"><circle style="fill:#EFCE4A;" cx="25" cy="25" r="25"/><line style="fill:none;stroke:#FFFFFF;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;" x1="25" y1="10" x2="25" y2="32"/><line style="fill:none;stroke:#FFFFFF;stroke-width:2;stroke-linecap:round;stroke-miterlimit:10;" x1="25" y1="37" x2="25" y2="39"/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>',
+
+	$infoSvg: '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"' +
+		'viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;width:30px;height:30px;" xml:space="preserve">' +
+		'<path style="fill:#00D2FF;" d="M256,512C114.844,512,0,397.156,0,256S114.844,0,256,0s256,114.844,256,256S397.156,512,256,512z"/>' +
+		'<path style="fill:#18BDF6;" d="M375.467,426.667c-141.156,0-256-114.844-256-256c0-59.087,20.318-113.41,54.071-156.783' +
+		'C72.768,48.311,0,143.72,0,256c0,141.156,114.844,256,256,256c82.069,0,155.049-38.974,201.929-99.217' +
+		'C432.012,421.638,404.342,426.667,375.467,426.667z"/>' +
+		'<g><path style="fill:#FFFFFF;" d="M291.31,397.241h-0.009c-4.875,0-8.828-3.953-8.828-8.828V203.034c0-4.875,3.953-8.828,8.828-8.828' +
+		'h0.009c4.879,0,8.828-3.953,8.828-8.828c0-4.875-3.948-8.828-8.828-8.828H220.69c-4.879,0-8.828,3.953-8.828,8.828' +
+		'c0,4.875,3.948,8.828,8.828,8.828h-0.009c4.875,0,8.828,3.953,8.828,8.828v185.379c0,4.875-3.953,8.828-8.828,8.828h0.009' +
+		'c-4.879,0-8.828,3.953-8.828,8.828s3.948,8.828,8.828,8.828h70.621c4.879,0,8.828-3.953,8.828-8.828S296.19,397.241,291.31,397.241' +
+		'z"/>' +
+		'<circle style="fill:#FFFFFF;" cx="256" cy="123.586" r="26.483"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>',
+
+	$errorSvg: '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"' +
+		' viewBox="0 0 507.2 507.2" style="enable-background:new 0 0 507.2 507.2;width:30px;height:30px;" xml:space="preserve">' +
+		'<circle style="fill:#F15249;" cx="253.6" cy="253.6" r="253.6"/>' +
+		'<path style="fill:#AD0E0E;" d="M147.2,368L284,504.8c115.2-13.6,206.4-104,220.8-219.2L367.2,148L147.2,368z"/>' +
+		'<path style="fill:#FFFFFF;" d="M373.6,309.6c11.2,11.2,11.2,30.4,0,41.6l-22.4,22.4c-11.2,11.2-30.4,11.2-41.6,0l-176-176' +
+		'c-11.2-11.2-11.2-30.4,0-41.6l23.2-23.2c11.2-11.2,30.4-11.2,41.6,0L373.6,309.6z"/>' +
+		'<path style="fill:#D6D6D6;" d="M280.8,216L216,280.8l93.6,92.8c11.2,11.2,30.4,11.2,41.6,0l23.2-23.2c11.2-11.2,11.2-30.4,0-41.6' +
+		'L280.8,216z"/>' +
+		'<path style="fill:#FFFFFF;" d="M309.6,133.6c11.2-11.2,30.4-11.2,41.6,0l23.2,23.2c11.2,11.2,11.2,30.4,0,41.6L197.6,373.6' +
+		'c-11.2,11.2-30.4,11.2-41.6,0l-22.4-22.4c-11.2-11.2-11.2-30.4,0-41.6L309.6,133.6z"/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>'
+
+
+}
 var globalObj = {
-	append : null,
-	$ref:null,
-	settingObj : null,
-	successColor : 'rgba(0,200,0,0.8)',
-	errorColor : 'rgba(200,0,0,0.8)',
-	infoColor : 'rgba(220,200,0,0.8)',
-	warningColor:'rgba(120,50,50,0.8)',
-	setWidth:function () {
-		this.$append.css('width',this.settingObj.width+'px');
+	append: null,
+	$ref: null,
+	settingObj: null,
+
+	successColor: 'rgba(0,200,0,0.8)',
+	errorColor: 'rgba(200,0,0,0.8)',
+	infoColor: 'rgba(220,200,0,0.8)',
+	warningColor: 'rgba(120,50,50,0.8)',
+	setWidth: function () {
+		this.$append.css('width', this.settingObj.width + 'px');
 	},
-	setHeight:function () {
-		this.$append.css('height',this.settingObj.height+'px');
+	setHeight: function () {
+		this.$append.css('height', this.settingObj.height);
 	},
-	setPosition:function(){
-		if(this.settingObj.top) this.$append.css('top',this.settingObj.top+'px');
-		if(this.settingObj.bottom) this.$append.css('bottom',this.settingObj.bottom+'px');
-		if(this.settingObj.right) this.$append.css('right',this.settingObj.right+'px');
-		if(this.settingObj.left) this.$append.css('left',this.settingObj.left+'px');
+	setPosition: function () {
+		if (this.settingObj.top) this.$append.css('top', this.settingObj.top + 'px');
+		if (this.settingObj.bottom) this.$append.css('bottom', this.settingObj.bottom + 'px');
+		if (this.settingObj.right) this.$append.css('right', this.settingObj.right + 'px');
+		if (this.settingObj.left) this.$append.css('left', this.settingObj.left + 'px');
 	},
-	setType:function(){
-		if(this.settingObj.type == 'success') this.$append.css('background-color',this.successColor);
-		if(this.settingObj.type == 'error') this.$append.css('background-color',this.errorColor);
-		if(this.settingObj.type == 'info') this.$append.css('background-color',this.infoColor);
-		if(this.settingObj.type == 'warning') this.$append.css('background-color',this.warningColor);
+	setType: function () {
+		if (this.settingObj.type == 'success') {
+			this.$append.css('background-color', this.successColor);
+		}
+		if (this.settingObj.type == 'error') {
+			this.$append.css('background-color', this.errorColor);
+		}
+		if (this.settingObj.type == 'info') {
+			this.$append.css('background-color', this.infoColor);
+		}
+		if (this.settingObj.type == 'warning') {
+			this.$append.css('background-color', this.warningColor);
+		}
 	},
-	addStyles:function(){
+	addStyles: function () {
 		this.setWidth();
 		this.setHeight();
 		this.setPosition();
 		this.setType();
 	},
-	showPopup : function(){
+	showPopup: function () {
 		this.$append.appendTo('body');
 		this.$ref = $('#js-show-Popup');
 		this.animate();
 	},
-	hidePopup : function(time){
+	hidePopup: function () {
 		var that = this;
-		setTimeout(function(){
-			if(that.settingObj.animate == 'fade' ) that.$ref.fadeOut('slow');
+		setTimeout(function () {
+			if (that.settingObj.animate == 'fade') that.$ref.fadeOut('slow');
 			else that.$ref.slideUp('slow');
-		},this.settingObj.time);
+		}, this.settingObj.time);
 	},
-	animate:function(){
-		if(this.settingObj.animate == 'fade' ) this.$ref.fadeIn('slow');
+	animate: function () {
+		if (this.settingObj.animate == 'fade') this.$ref.fadeIn('slow');
 		else this.$ref.slideDown('slow');
-		this.hidePopup();
+		if (!this.settingObj.sticky) this.hidePopup();
 	},
-	createPopup:function(){
-		this.$append = $('<div id="js-show-Popup" class="default-css-popup">'+this.settingObj.msg+'</div>');
+	createPopup: function () {
+		this.$append = $('<div id="js-show-Popup" class="default-css-popup">' +
+			'<p class="close-popup-icon-button"><span id="popup-close-button">' + allSvg.$closeSvg + '</span></p>' +
+			'<br/><p class="popup-data-container"><span class="all-other-popup-icons">' + this.settingObj.svg +
+			'</span><span class="all-other-popup-data">' + this.settingObj.msg + '</span></p>' +
+			'</div>');
 	},
-	attachDefaultValues:function(settingObj){
-		if(!settingObj.msg) settingObj.msg = 'Put Your Message Here';
-		if(!settingObj.width) settingObj.width = 200;
-		if(!settingObj.height) settingObj.height = 50;
-		if(settingObj.position){
-			if(settingObj.position == 'top-left'){
+	handleCloseButtonClick: function () {
+		var that = this;
+		$('body').on('click', '#popup-close-button', function () {
+			that.settingObj.time = 0;
+			that.hidePopup();
+		})
+	},
+	attachDefaultValues: function (settingObj) {
+		if (!settingObj.msg) settingObj.msg = 'Put Your Message Here';
+		if (!settingObj.width) settingObj.width = 200;
+		if (!settingObj.height) settingObj.height = 'inherit';
+		else settingObj.height = settingObj.height + "px";
+		if (settingObj.position) {
+			if (settingObj.position == 'top-left') {
 				settingObj.top = 10;
-				settingObj.left= 10;
+				settingObj.left = 10;
 			}
-			if(settingObj.position == 'top-right'){
+			if (settingObj.position == 'top-right') {
 				settingObj.top = 10;
-				settingObj.right= 10;
+				settingObj.right = 10;
 			}
-			if(settingObj.position == 'bottom-left'){
+			if (settingObj.position == 'bottom-left') {
 				settingObj.bottom = 10;
-				settingObj.left= 10;
+				settingObj.left = 10;
 			}
-			if(settingObj.position == 'bottom-right'){
+			if (settingObj.position == 'bottom-right') {
 				settingObj.bottom = 10;
-				settingObj.right= 10;
+				settingObj.right = 10;
 			}
-		}else{
+		} else {
 			settingObj.top = 10;
-			settingObj.right= 20;
+			settingObj.right = 20;
 		}
-		if(!settingObj.type) settingObj.type='success';
-		if(!settingObj.animate) settingObj.type='slide';
-		if(!settingObj.time) settingObj.time = 5000;
-		this.settingObj=settingObj;
+		if (!settingObj.type) settingObj.type = 'success';
+		if (settingObj.type == 'success')
+			settingObj.svg = allSvg.$successSvg;
+		if (settingObj.type == 'error')
+			settingObj.svg = allSvg.$errorSvg;
+		if (settingObj.type == 'info')
+			settingObj.svg = allSvg.$infoSvg;
+		if (settingObj.type == 'warning')
+			settingObj.svg = allSvg.$warningSvg;
+		if (!settingObj.animate) settingObj.type = 'slide';
+		if (!settingObj.time) settingObj.time = 5000;
+		if (!settingObj.sticky) settingObj.sticky = false;
+		this.settingObj = settingObj;
 	},
-	init:function (settingObj) {
+	init: function (settingObj) {
 		this.attachDefaultValues(settingObj);
 		this.createPopup();
 		this.addStyles();
 		this.showPopup();
+		this.handleCloseButtonClick();
 	}
 }
-var $popup ={
+var $popup = {
 
-	show:function(settingObj){
+	show: function (settingObj) {
+		$('#js-show-Popup').remove();
 		globalObj.init(settingObj);
 		console.error("error my mind");
-		//globalObj.init(settingObj);
 	}
 }
